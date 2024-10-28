261bcb65f9a6d7edfdd863e2f362887e7613bf3c
Muhannad <muhanned9980@gmail.com>
2024-10-28 15:12:32 +0300

    adding the navbar and modify the unexpected long number bug

--- controllers/books.js
+++ controllers/books.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const User = require("../models/user");
 const Book = require("../models/book");
+const book = require("../models/book");
 
 // Index
 router.get("/", async (req, res) => {
@@ -16,25 +17,37 @@ router.get("/new", async (req, res) => {
 });
 //create
 router.post("/", async (req, res) => {
+  if (req.body.price > 0 && req.body.price < 10000000000000) {
   req.body.owner = req.session.user._id;
   await Book.create(req.body);
   res.redirect("/books");
+  }
+  else {
+    res.redirect('/books')
+  }
 });
+
 //show
 router.get("/:bookId", async (req, res) => {
-  const book = await Book.findById(req.params.bookId);
+  try {
+    const populatedBooks = await Book.findById(req.params.bookId).populate(
+      "owner"
+    );
 
-  res.render("books/show.ejs", { book });
+    res.render("books/show.ejs", { book: populatedBooks });
+  } catch (error) {
+    console.log(error);
+    res.redirect("/");
+  }
 });
 
 //edit
 // controller to edit the listing
 router.get("/:bookId/edit", async (req, res) => {
   try {
-    const currentBook = await Book.findById(req.params.bookId);
-    res.render("books/edit.ejs", {
-      book: currentBook,
-    });
+      const currentBook = await Book.findById(req.params.bookId);
+      res.render("books/edit.ejs", {book: currentBook});
+  
   } catch (error) {
     console.log(error);
     res.redirect("/");
@@ -49,7 +62,7 @@ router.put("/:bookId", async (req, res) => {
       await currentBook.updateOne(req.body);
       res.redirect("/books");
     } else {
-      res.send("You don't have permission to do that.");
+     res.redirect("/books");
     }
   } catch (error) {
     console.log(error);
@@ -64,15 +77,13 @@ router.delete("/:bookId", async (req, res) => {
     if (book.owner.equals(req.session.user._id)) {
       await book.deleteOne();
       res.redirect("/books");
-    }
-    else{
+    } else {
       res.send("You don't have permission to do that.");
     }
   } catch (error) {
     console.error(error);
     res.redirect("/");
   }
-
 });
 
 module.exports = router;
